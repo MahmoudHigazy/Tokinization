3b29dc84db3ba36ffb1a0fd9d6830b46d17cfa70
Mahmoud Higazy <ma7moudhigazy@gmail.com>
2022-02-16 17:02:16 +0200

    adde Crowdsal contract

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,14 +1,18 @@
-pragma solidity >=0.6.0;
+pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Crowdsale.sol";
 
-contract MyToken is ERC20 {
+contract MyTokenSale is Crowdsale {
 
-    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") public {
-
-        _mint(msg.sender, initialSupply);
-
-    _setupDecimals(0);
+    KycContract kyc;
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
 
     }
 
